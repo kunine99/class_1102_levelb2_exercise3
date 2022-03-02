9fea8e09c85c762b73303a80308cb5c8f7dda21f
rong <rongyy94job@gmail.com>
2022-03-02 11:06:34 +0800

    使用dreamwaver及在首頁切版

--- front/header.php
+++ front/header.php
@@ -1,5 +1,8 @@
-<div id="title">
-    00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+<div id="title" style="display: inline;">
+   <?=date("m 月 y 號 ");?>| 
+   今日瀏覽: 1 | 
+   累積瀏覽: 36 </div>
+   <a href="index.php" style="float: right;">回首頁</a>
 <div id="title2">
     <a href="index.php" title="健康促進網-回首頁">
         <img src="./icon/02B01.jpg" alt="">
